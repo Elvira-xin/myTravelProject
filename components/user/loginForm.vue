58e590f30faee851175d93fa9f3b455386204433
zhouxin <840137893@qq.com>
2019-09-04 09:14:03 +0800

    登录页面结构样式完成

---
+++ components/user/loginForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <el-form ref="form" :model="form" :rules="rules" class="form">
+    <el-form-item prop="username" class="form-item">
+      <el-input v-model="form.username" placeholder="用户名/手机" />
+    </el-form-item>
+    <el-form-item prop="password" class="form-item">
+      <el-input v-model="form.password" placeholder="密码" />
+    </el-form-item>
+    <span class="form-text" style="margin-left:300px"><nuxt-link to="#">忘记密码</nuxt-link></span>
+    <el-form-item>
+      <el-button class="submit" @click="handleLoginSubmit">
+        登录
+      </el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        username: '13800138000',
+        password: '123456'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleLoginSubmit () {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$axios({
+            url: '/accounts/login',
+            method: 'post',
+            data: this.form
+          }).then((res) => {
+            // console.log(res)
+            this.$message.success('登录成功')
+            this.$router.push({ name: 'index' })
+          })
+            .catch(() => {
+              this.$message.error('登录失败')
+            })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.form {
+  padding: 25px;
+}
+
+.form-item {
+  margin-bottom: 20px;
+}
+
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
+
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
+</style>
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -1,13 +1,78 @@
 <template>
-  <div>登录页</div>
+  <div class="container">
+    <!--表单-->
+    <el-row class="main" justify="center" type="flex" align="middle">
+      <div class="form-wrapper">
+        <el-row class="tabs" justify="center" type="flex">
+          <span
+            v-for="(item,index) in ['登录','注册']"
+            :key="index"
+            :class="{active:current===index}"
+            @click="handleClick(index)"
+          >{{ item }}</span>
+        </el-row>
+        <!--登录组件-->
+        <LoginForm v-if="current== 0" />
+      </div>
+    </el-row>
+  </div>
 </template>
 
 <script>
+// 注册登录组件
+import LoginForm from '@/components/user/loginForm.vue'
 export default {
-
+  components: { LoginForm },
+  data () {
+    return {
+      current: 0
+    }
+  },
+  methods: {
+    handleClick (index) {
+      this.current = index
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container {
+  background: url(http://157.122.54.189:9095/assets/images/th03.jfif) center 0;
+  height: 700px;
+  min-width: 1000px;
+  .main {
+    width: 1000px;
+    height: 100%;
+    margin: 0 auto;
+    position: relative;
+    .form-wrapper {
+      width: 400px;
+      margin: 0 auto;
+      background: #fff;
+      box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+      .tabs {
+        span {
+          display: block;
+          width: 50%;
+          height: 50px;
+          box-sizing: border-box;
+          border-top: 2px #eee solid;
+          background: #eee;
+          line-height: 48px;
+          text-align: center;
+          cursor: pointer;
+          color: #666;
+          &.active {
+            color: orange;
+            border-top-color: orange;
+            background: #fff;
+            font-weight: bold;
+          }
+        }
+      }
+    }
+  }
+}
 </style>
